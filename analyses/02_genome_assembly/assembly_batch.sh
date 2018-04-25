a5e754a527c20c746ab3bbc10f12184e2b6dd1e3
Caitlin Vigetun Haughey <caithaug@r34.uppmax.uu.se>
2018-04-25 15:35:16 +0200

    Ignored assembly files

--- analyses/02_genome_assembly/assembly_batch.sh
+++ analyses/02_genome_assembly/assembly_batch.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -l
+
 #SBATCH -A g2018003
+
 #SBATCH -p core
-#SBATCH -n 2
+
+#SBATCH -n 6
+
 #SBATCH -t 10:00:00
+
 #SBATCH -J assembly_job
+
 #SBATCH --mail-type=ALL
+
 #SBATCH --mail-user caitlinvigetun@hotmail.com
+
 # Load modules
+
 module load bioinfo-tools
+
 module load soapdenovo
+
 # Your commands
-SOAPdenovo-127mer all -s soapdenovo.contig -K 127 -R -o output-files/assembly_output_graph 1>ass.log 2>ass.err
+
+SOAPdenovo-127mer all -s soapdenovo.contig -K 49 -o output-files/assembly_output_graph -p 6 1>ass.log 2>ass.err
+
